5f7a115f8784a759af86b427240d10489d755b36
versechen <WeiCsir@outlook.com>
2025-07-01 07:30:06 +0800

    add comment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,13 @@ import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap(), react({include:['**/react/*'], experimentalReactChildren: true})],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), react({
+    include: ['**/react/*'],
+    experimentalReactChildren: true
+  }), preact()]
 });
