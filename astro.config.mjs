db0c8198397bdbcf7442cd53bced280e5f4c8b08
versechen <WeiCsir@outlook.com>
2026-02-25 21:09:13 +0800

    feat: github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://versechen.github.io',
   integrations: [mdx(), sitemap(), react({
     include: ['**/react/*'],
     experimentalReactChildren: true
